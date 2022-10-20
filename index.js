4d40a80e00fcc868799d3b0d7ec53b8f3a6c5f3f
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-20 20:45:18 +0600

    Basic API implementation done

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API paoa gece');
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const categoryId = req.params.id;
+    if (categoryId === '08') {
+        res.send(news);
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === categoryId);
+        res.send(selectedCategory);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const newsId = req.params.id;
+    const selectedNews = news.find(n => n._id === newsId);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
